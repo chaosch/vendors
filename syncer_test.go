4c60dc851e60835ab49171a7643ba0e82cad7379
黎东海 <jacle169@21cn.com>
2017-12-13 13:06:51 +0800

    Squashed 'github.com/jacoblai/yiyirpc/' content from commit ac9c803
    git-subtree-dir: github.com/jacoblai/yiyirpc
    git-subtree-split: ac9c803f49eff52fe060e38298c2656750c9aa96

---
+++ syncer_test.go
@@ -0,0 +1,51 @@
+package yiyirpc
+
+import (
+	"testing"
+	"time"
+	"fmt"
+	"log"
+	"io/ioutil"
+)
+
+func TestListenRPC(t *testing.T) {
+	srv := RpcServer{}
+	srv.Register(NewWorker())
+	go srv.ListenRPC(":4200")
+	N := 10
+	mapChan := make(chan int, N)
+	bts, _:= ioutil.ReadFile(`C:\Users\jac\Desktop\缺失资料\缺失资料.zip`)
+	for i := 0; i < N; i++ {
+		go func(i int) {
+			client := RpcCleint{}
+			var rep []byte
+			nt := time.Now()
+			err := client.Call("localhost:4200", "Worker.DoJob", bts, &rep)
+			if err != nil {
+				t.Error(err)
+			} else {
+				sub := time.Now().Sub(nt)
+				fmt.Println(i, string(rep), sub)
+			}
+			mapChan <- i
+		}(i)
+	}
+	for i := 0; i < N; i++ {
+		<-mapChan
+	}
+}
+
+type Worker struct {
+	Name string
+}
+
+func NewWorker() *Worker {
+	return &Worker{"test"}
+}
+
+func (w *Worker) DoJob(task []byte, reply *[]byte) error {
+	log.Println("Worker: do job", len(task))
+	//time.Sleep(time.Second * 3)
+	*reply = []byte("OK")
+	return nil
+}
