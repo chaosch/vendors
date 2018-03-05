d9b9390f50fdbb7ab404151f8bd5790ac995b821
chaoschaime <chaosch@163.com>
2018-03-05 14:01:03 +0800

    加入egov通用包

---
+++ egov/common/resultor.go
@@ -0,0 +1,100 @@
+package common
+
+import (
+	"log"
+	"reflect"
+	"runtime"
+	"encoding/json"
+)
+
+type ErrContext interface {
+	err() *errType
+}
+
+type errType struct {
+	ErrCode int `json:"code" msgpack:"code"`
+	ErrMsg  string `json:"msg" msgpack:"msg"`
+	ErrLine int `json:"line" msgpack:"line"`
+	ErrFile string `json:"file" msgpack:"file"`
+}
+
+var Es = map[int]string{
+	//0:    "normal error",
+	401:  "error token",
+	1001: "operate not support",
+	1002: "table null",
+	1003: "record ext",
+	1004: "record not ext",
+	1005: "operation return error",
+	1006: "where not v3 format",
+	8005: "can't find token",
+	8006: "access_token timeout!",
+	8007: "invalid token type,need access token!",
+	8008: "user not found!",
+	7001: "dictionary api error",
+	9000: "database error",
+	6001: "network error",
+}
+
+func (e *errType) err() *errType {
+	return e
+}
+
+func NewError(errorCode int,errorMsg string) ErrContext {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+	return &errType{ErrCode: errorCode, ErrMsg: errorMsg,ErrLine:line,ErrFile:file}
+}
+
+
+
+func RetChanges(changes int64) *resultTemplate {
+	res := &resultTemplate{Ok: true}
+	res.Changes = changes
+	return res
+}
+
+
+
+func RetErr(err ErrContext) *resultTemplate {
+	res := &resultTemplate{Ok: false}
+	if value, ok := Es[err.err().ErrCode]; ok {
+		res.Err = NewError(err.err().ErrCode, value+":"+err.err().ErrMsg)
+		x, _ := json.Marshal(res.Err)
+		log.Println(string(x))
+		return res
+	} else {
+		res.Err = NewError(0, value+":"+err.err().ErrMsg)
+		x, _ := json.Marshal(res.Err)
+		log.Println(string(x))
+		return res
+	}
+}
+
+type resultTemplate struct {
+	Ok      bool        `json:"ok" msgpack:"ok"`
+	Err     ErrContext  `json:"err" msgpack:"err"`
+	Changes int64       `json:"changes" msgpack:"changes"`
+	Data    interface{} `json:"data" msgpack:"data"`
+}
+
+func RetOk(result interface{}) *resultTemplate {
+	res := &resultTemplate{Ok: true}
+	if result == nil {
+		res.Changes = int64(0)
+		res.Err = nil
+		res.Data = []interface{}{nil}
+	}
+	resValue := reflect.ValueOf(result)
+	if resValue.Kind() == reflect.Array || resValue.Kind() == reflect.Slice {
+		res.Data = result
+		res.Changes = int64(resValue.Len())
+	} else {
+		res.Data = []interface{}{result}
+		res.Changes = int64(1)
+	}
+	return res
+}
