6af1a4585909c042c7572de859a9593d8da23880
chaoschaime <chaosch@163.com>
2018-04-11 11:33:29 +0800

    comment #IIIK3 修改通用日志接口

---
+++ egov/common/logtype.go
@@ -0,0 +1,25 @@
+package common
+
+import "time"
+
+type ProcessStatus struct {
+	InBuf         string     `json:"inbuf"`
+	Starttime     time.Time  `json:"starttime"`
+	EndTime       time.Time  `json:"endtime"`
+	Duration      int64      `json:"duration"`
+	OK            bool       `json:"ok"`
+	Err           ErrContext `json:"err"`
+	SqlDuration   int64      `json:"sqlduration"`
+	Changes       int64      `json:"changes"`
+	ChipId        int64      `json:"chipid"`
+	DalVersion    string     `json:"dalversion"`
+	IpAddress     string     `json:"ipaddress"`
+	InTransaction bool       `json:"intransaction"`
+	Prompt        string     `json:"prompt"`
+}
+
+type LogData struct {
+	system  string
+	kind    string
+	content interface{}
+}
