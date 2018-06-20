219596a693c427984dbdfd833c8d3713a2736b23
chaoschaime <chaosch@163.com>
2018-06-20 16:06:39 +0800

    表定义放入vendor

--- egov/common/check.go
+++ egov/common/check.go
@@ -169,3 +169,20 @@ func PrintGreen(format string, a ...interface{}){
 func Print(format string, a ...interface{}){
 	Println(0,format,a...)
 }
+
+type Boolean bool
+
+func (bit *Boolean) UnmarshalJSON(data []byte) error {
+	asString := string(data)
+	//	asString = strings.Replace(asString,"\"","",-1)
+	//	var tempValue Boolean
+	if asString == "1" || asString == "true" {
+		*bit = true
+	} else if asString == "0" || asString == "false" {
+		*bit = false
+	} else {
+		return errors.New(fmt.Sprintf("Boolean unmarshal error: invalid input %s", asString))
+	}
+	return nil
+}
+
