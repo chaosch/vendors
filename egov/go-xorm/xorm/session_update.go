12e922aa59731d75d18682ca141db4ae2685d15a
chaoschaime <chaosch@163.com>
2018-07-11 15:21:48 +0800

    数据状态默认改为'N'，取消自增长字段修改可能

--- egov/go-xorm/xorm/session_update.go
+++ egov/go-xorm/xorm/session_update.go
@@ -179,7 +179,7 @@ func (session *Session) Update(bean interface{}, condiBean ...interface{}) (int6
 
 		if session.Statement.ColumnStr == "" {
 			colNames, args = buildUpdates(session.Engine, session.Statement.RefTable, bean, false, false,
-				false, true, session.Statement.allUseBool, session.Statement.useAllCols,
+				false, false, session.Statement.allUseBool, session.Statement.useAllCols,
 				session.Statement.mustColumnMap, session.Statement.nullableMap,
 				session.Statement.columnMap, true, session.Statement.unscoped)
 		} else {
