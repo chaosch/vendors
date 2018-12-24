56d5f7601f1ffa5dab133b1c27dce01521646d2c
chaosch@163.com <ch740205>
2018-12-24 23:55:57 +0800

    tab_region_ex 增加 department和area_code tab_affairs_info_ex 增加area_code tab_affairs_mark_result_ex增加area_code

--- egov/go-xorm/xorm/engine.go
+++ egov/go-xorm/xorm/engine.go
@@ -1551,6 +1551,9 @@ func (engine *Engine) SyncFast(tableMaps map[string]map[string]*core.Column, bea
 					}
 				} else {
 					if strings.Trim(col.XormTag ,"")!=strings.Trim(phyCol.XormTag,""){
+						fmt.Println()
+						fmt.Println(col.XormTag)
+						fmt.Println(phyCol.XormTag)
 						sqls := engine.dialect.ModifyColumnSql(table.Name, col)
 						for _, sql := range strings.Split(sqls, ";") {
  							engine.ShowSQL(true)
