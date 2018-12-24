93f7f979835b54ed3336757120c504bcef7a8011
chaosch@163.com <ch740205>
2018-12-25 01:21:32 +0800

    tab_region_ex 增加 department和area_code tab_affairs_info_ex 增加area_code tab_affairs_mark_result_ex增加area_code

--- egov/go-xorm/core/dialect.go
+++ egov/go-xorm/core/dialect.go
@@ -693,9 +693,9 @@ func TransMapStringColumn(maxColLen int, column map[string]string) (string, *Col
 
 	content += "`\n"
 
-	if col.FieldName=="data_interface"||col.FieldName=="need_dist"{
+	//if col.FieldName=="data_interface"||col.FieldName=="need_dist"{
 		fmt.Println(col.XormTag)
-	}
+	//}
 	return content, col
 
 }
