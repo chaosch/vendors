71b894d35adf9e74513daad2b973e1b0cf46ce44
chaosch@163.com <ch740205>
2019-04-10 10:35:29 +0800

    改变idg id生成规则 改回二进制7+7+7+序列

--- egov/common/construct_idprefix.go
+++ egov/common/construct_idprefix.go
@@ -1,24 +1,36 @@
 package common
 
-import "math"
-
 func Construct(pid, cid, did int64) int64 {
 	Idprefix := int64(0)
 	//	Idprefix = int64(pid+cid+did) << 32
-	Idprefix = int64(pid*(int64(math.Pow10(14))) + cid*int64(math.Pow10(12)) + (did * int64(math.Pow10(10))))
+	//Idprefix = int64(pid*(int64(math.Pow10(14))) + cid*int64(math.Pow10(12)) + (did * int64(math.Pow10(10))))
+
+	//Id.Provice_id=pid//
+	//Id.City_id=cid//
+	//Id.District_id=did//
+	//Id.Idprefix=0
+	//Id.Provice_id=Id.Provice_id<<15
+	//Id.City_id=Id.City_id<<8
+	if pid >= 80 {
+		pid = pid - 55
+	}
+	Idprefix = int64(pid<<15+cid<<8+did) << 32
 
 	return Idprefix
 }
 
 func ConstructNextPrefix(pid, cid, did int64) int64 {
+	if pid >= 80 {
+		pid = pid - 55
+	}
 	Idprefix := int64(0)
 	if pid == 0 {
-		Idprefix = int64(1*(int64(math.Pow10(14))) + (1)*int64(math.Pow10(12)) + int64(math.Pow10(10)))
+		Idprefix = int64((1<<15 + 1<<8 + 1) << 32)
 	} else {
 		if cid == 0 {
-			Idprefix = int64(pid*(int64(math.Pow10(14))) + (cid+1)*int64(math.Pow10(12)) + int64(math.Pow10(10)))
+			Idprefix = int64((pid<<15 + (cid+1)<<8 + 1) << 32)
 		} else {
-			Idprefix = int64(pid*(int64(math.Pow10(14))) + cid*int64(math.Pow10(12)) + ((did + 1) * int64(math.Pow10(10))))
+			Idprefix = int64((pid<<15 + cid<<8 + (did + 1)) << 32)
 		}
 	}
 
--- egov/go-xorm/core/identify.go
+++ egov/go-xorm/core/identify.go
@@ -3,35 +3,37 @@ package core
 import (
 	"sync"
 	"time"
-	"math"
 )
+
 var index int64 = 1
 var mutex sync.Mutex
-var (Idt=&Identify{}
+var (
+	Idt = &Identify{}
 )
 
-
 type Identify struct {
-	Count uint64
-	Idprefix int64
-	Provice_id uint64
-	City_id uint64
+	Count       uint64
+	Idprefix    int64
+	Provice_id  uint64
+	City_id     uint64
 	District_id uint64
 }
 
-
-func (Id *Identify) Construct(pid,cid,did uint64){
-	Id.Provice_id=pid//
-	Id.City_id=cid//
-	Id.District_id=did//
-	Id.Idprefix=0
-	//Id.Provice_id=Id.Provice_id<<15
-	//Id.City_id=Id.City_id<<8
-	//Id.Idprefix=int64(Id.Provice_id+Id.City_id+Id.District_id)<<32
-	Id.Idprefix=int64(Id.Provice_id*(uint64(math.Pow10(14)))+Id.City_id*uint64(math.Pow10(12))+(Id.District_id*uint64(math.Pow10(10))))
+func (Id *Identify) Construct(pid, cid, did uint64) {
+	Id.Provice_id = pid //
+	if Id.Provice_id >= 80 {
+		Id.Provice_id = Id.Provice_id - 55
+	}
+	Id.City_id = cid     //
+	Id.District_id = did //
+	Id.Idprefix = 0
+	Id.Provice_id = Id.Provice_id << 15
+	Id.City_id = Id.City_id << 8
+	Id.Idprefix = int64(Id.Provice_id+Id.City_id+Id.District_id) << 32
+	//Id.Idprefix=int64(Id.Provice_id*(uint64(math.Pow10(14)))+Id.City_id*uint64(math.Pow10(12))+(Id.District_id*uint64(math.Pow10(10))))
 }
 
-func (ID *Identify)GetId() int64 {
+func (ID *Identify) GetId() int64 {
 	var indexTemp int64 = 1
 	var ticksTemp int64 = 1
 	mutex.Lock()
