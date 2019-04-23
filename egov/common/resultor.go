57070fd422ef24c6ede44a93429a4176b970b644
chaosch@163.com <ch740205>
2019-04-23 15:51:33 +0800

    dal增加默认路由显示内容,命令行参数和配置文件内容

--- egov/common/resultor.go
+++ egov/common/resultor.go
@@ -1,13 +1,12 @@
 package common
 
 import (
+	"encoding/json"
 	"log"
 	"reflect"
 	"runtime"
-	"encoding/json"
 	"strings"
 )
-
 
 var Es = map[int]string{
 	//0:    "normal error",
@@ -31,25 +30,26 @@ var Es = map[int]string{
 type ResultTemplate struct {
 	//成功
 	//example: false
-	Ok          bool        `json:"ok" msgpack:"ok"`
+	Ok bool `json:"ok" msgpack:"ok"`
 	//错误对象
-	Err         ErrContext  `json:"err" msgpack:"err"`
+	Err ErrContext `json:"err" msgpack:"err"`
 	//影响记录数
 	//example: 0
-	Changes     int64       `json:"changes" msgpack:"changes"`
+	Changes int64 `json:"changes" msgpack:"changes"`
 	//耗时
 	//example: 1000000
-	Duration    int64       `json:"duration" msgpack:"duration"`
+	Duration int64 `json:"duration" msgpack:"duration"`
 	//sql语句耗时
 	//example: 1000000
-	SqlDuration int64       `json:"sql_duration" msgpack:"sql_duration"`
+	SqlDuration int64 `json:"sql_duration" msgpack:"sql_duration"`
 	//返回数据
-	Data        interface{} `json:"data" msgpack:"data"`
+	Data interface{} `json:"data" msgpack:"data"`
 	//成功提示:
 	//example:
-	OKMessage   string      `json:"ok_message"`
-}
+	OKMessage string `json:"ok_message"`
 
+	TransList interface{} `json:"trans_list"`
+}
 
 type ErrContext interface {
 	Err() *ErrType
@@ -61,7 +61,7 @@ type ErrType struct {
 	ErrCode int `json:"code" msgpack:"code"`
 	//错误消息
 	//example: table null
-	ErrMsg  string `json:"msg" msgpack:"msg"`
+	ErrMsg string `json:"msg" msgpack:"msg"`
 	//出错行号
 	//example: 35
 	ErrLine int `json:"line" msgpack:"line"`
@@ -70,31 +70,27 @@ type ErrType struct {
 	ErrFile string `json:"file" msgpack:"file"`
 }
 
-
-
 func (e *ErrType) Err() *ErrType {
 	return e
 }
 
-func NewError(errorCode int,errorMsg string) ErrContext {
+func NewError(errorCode int, errorMsg string) ErrContext {
 	_, file, line, ok := runtime.Caller(2)
 	if !ok {
 		file = "???"
 		line = 0
 	}
-	return &ErrType{ErrCode: errorCode, ErrMsg: errorMsg,ErrLine:line,ErrFile:file}
+	return &ErrType{ErrCode: errorCode, ErrMsg: errorMsg, ErrLine: line, ErrFile: file}
 }
 
-
-func NewError0(errorCode int,errorMsg string) ErrContext {
+func NewError0(errorCode int, errorMsg string) ErrContext {
 	_, file, line, ok := runtime.Caller(0)
 	if !ok {
 		file = "???"
 		line = 0
 	}
-	return &ErrType{ErrCode: errorCode, ErrMsg: errorMsg,ErrLine:line,ErrFile:file}
+	return &ErrType{ErrCode: errorCode, ErrMsg: errorMsg, ErrLine: line, ErrFile: file}
 }
-
 
 func RetChanges(changes int64) *ResultTemplate {
 	res := &ResultTemplate{Ok: true}
@@ -105,10 +101,9 @@ func RetChanges(changes int64) *ResultTemplate {
 func RetChangesStr(changes int64) string {
 	res := &ResultTemplate{Ok: true}
 	res.Changes = changes
-	str,_:=json.Marshal(res)
+	str, _ := json.Marshal(res)
 	return string(str)
 }
-
 
 func RetErr(err ErrContext) *ResultTemplate {
 	res := &ResultTemplate{Ok: false}
@@ -125,8 +120,6 @@ func RetErr(err ErrContext) *ResultTemplate {
 	}
 }
 
-
-
 func RetErrStr(err ErrContext) string {
 	res := &ResultTemplate{Ok: false}
 	if value, ok := Es[err.Err().ErrCode]; ok {
@@ -141,8 +134,6 @@ func RetErrStr(err ErrContext) string {
 		return string(x)
 	}
 }
-
-
 
 func RetOk(result interface{}) *ResultTemplate {
 	res := &ResultTemplate{Ok: true}
@@ -162,7 +153,6 @@ func RetOk(result interface{}) *ResultTemplate {
 	return res
 }
 
-
 func RetOkStr(result interface{}) string {
 	res := &ResultTemplate{Ok: true}
 	if result == nil {
@@ -178,10 +168,9 @@ func RetOkStr(result interface{}) string {
 		res.Data = []interface{}{result}
 		res.Changes = int64(1)
 	}
-	str,_:=json.Marshal(res)
+	str, _ := json.Marshal(res)
 	return string(str)
 }
-
 
 func MixErrContext(errs ...ErrContext) ErrContext {
 	errMessage := ""
