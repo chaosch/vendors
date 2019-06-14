72499c9af64b07014bae178e197626fa66d210d6
chaosch@163.com <ch740205>
2019-06-14 16:57:11 +0800

    优化查询结果，某些情况下不进行翻译流程

--- github.com/binlaniua/SqlParser/test/parser_test.go
+++ github.com/binlaniua/SqlParser/test/parser_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"github.com/binlaniua/SqlParser"
 	"testing"
 )
@@ -33,7 +32,59 @@ func TestSelect(t *testing.T) {
 	//`)
 
 	p := sqlparse.NewSQLParser(
-		`select a,count(*) x from b group by a
+		`SELECT
+	b.affairs_info_id
+FROM
+	tab_affairs_node_status AS a
+INNER JOIN tab_affairs_info AS b ON b.affairs_info_id = a.affairs_info_id
+INNER JOIN tab_affairs AS c ON c.affairs_id = b.affairs_id
+LEFT JOIN tab_affairs_proposers AS e ON e.affairs_info_id = b.affairs_info_id
+AND e.primary_proposer = 1
+INNER JOIN (
+	SELECT
+		au2.node_id
+	FROM
+		tab_authorization AS au2
+	WHERE
+		(
+			au2.user_id = 22
+			AND au2.enable_mark = 1
+			AND au2.delete_mark = 0
+		)
+	UNION
+		SELECT
+			au1.node_id
+		FROM
+			tab_authorization AS au1
+		INNER JOIN tab_group_user_relation AS urg ON urg.group_id = au1.group_id
+		AND urg.user_id = 22
+		AND urg.enable_mark = 1
+		AND urg.delete_mark = 0
+		AND au1.enable_mark = 1
+		AND au1.delete_mark = 0
+) AS au ON au.node_id = a.node_id
+WHERE
+	(
+		(
+			(
+				a.enable_mark = 1
+				AND a.delete_mark = 0
+			)
+			AND a. STATUS = '2'
+		)
+		AND a.delete_mark = 0
+	)
+GROUP BY
+	b.affairs_info_id,
+	b.affairs_info_code,
+	c.affairs_id,
+	c.affairs_name,
+	b.affairs_start,
+	a. STATUS,
+	b.user_name,
+	e.proposer_name,
+	a.status_id,
+	a.node_id
 `)
 	r, err := p.DoParser()
 	if err != nil {
@@ -41,8 +92,8 @@ func TestSelect(t *testing.T) {
 	} else {
 		t.Log(r.String())
 	}
-	fmt.Println(p.GetResult().GetDBUser("*").TableMap["b"].GetTopAlias())
-	fmt.Println(p.GetResult().GetDBUser("*").TableMap["b"].ColumnMap["a"].GetTopAlias())
+	//fmt.Println(p.GetResult().GetDBUser("*").TableMap["b"].GetTopAlias())
+	//fmt.Println(p.GetResult().GetDBUser("*").TableMap["b"].ColumnMap["a"].GetTopAlias())
 
 }
 
