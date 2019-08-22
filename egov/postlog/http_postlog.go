96714c195678504d39db66a5d6b6abee3fd693c1
chaosch@163.com <ch740205>
2019-08-22 23:23:17 +0800

    增加文件日志功能日志

--- egov/postlog/http_postlog.go
+++ egov/postlog/http_postlog.go
@@ -261,19 +261,19 @@ type LogStructSlice struct {
 }
 
 type ProcessStatus struct {
-	InBuf         string                 `json:"inbuf;omitempty"`
-	InBufObject   map[string]interface{} `json:"inBufObject;omitempty" `
-	Starttime     time.Time              `json:"starttime;omitempty"`
-	EndTime       time.Time              `json:"endtime;omitempty"`
-	CreateTime    time.Time              `json:"create_time;omitempty"` //必须
-	Duration      int64                  `json:"duration;omitempty"`
-	OK            bool                   `json:"ok;omitempty"`
-	Err           interface{}            `json:"err;omitempty"`
-	SqlDuration   int64                  `json:"sqlduration;omitempty"`
-	Changes       int64                  `json:"changes;omitempty"`
-	ChipId        int64                  `json:"chipid;omitempty"`        //唯一标识
-	Version       string                 `json:"version;omitempty"`       //版本
-	IpAddress     string                 `json:"ipaddress;omitempty"`     //ip地址
-	InTransaction bool                   `json:"intransaction;omitempty"` //必须
-	Prompt        string                 `json:"prompt;omitempty"`
+	InBuf         string                 `json:"inbuf,omitempty"`
+	InBufObject   map[string]interface{} `json:"inBufObject,omitempty" `
+	Starttime     *time.Time              `json:"starttime,omitempty"`
+	EndTime       *time.Time              `json:"endtime,omitempty"`
+	CreateTime    *time.Time              `json:"create_time,omitempty"` //必须
+	Duration      int64                  `json:"duration,omitempty"`
+	OK            bool                   `json:"ok,omitempty"`
+	Err           interface{}            `json:"err,omitempty"`
+	SqlDuration   int64                  `json:"sqlduration,omitempty"`
+	Changes       int64                  `json:"changes,omitempty"`
+	ChipId        int64                  `json:"chipid,omitempty"`        //唯一标识
+	Version       string                 `json:"version,omitempty"`       //版本
+	IpAddress     string                 `json:"ipaddress,omitempty"`     //ip地址
+	InTransaction bool                   `json:"intransaction,omitempty"` //必须
+	Prompt        string                 `json:"prompt,omitempty"`
 }
