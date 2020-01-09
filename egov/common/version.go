9754ec8329caad1b7a8ca36805cc6c6b6f6099d2
chaosch@163.com <ch740205>
2020-01-09 11:41:08 +0800

    vender增加生成版本号方法

---
+++ egov/common/version.go
@@ -0,0 +1,17 @@
+package common
+
+import "strings"
+
+func MakeVersion(branch, build string) string {
+	version := ""
+	branch = strings.ToLower(branch)
+	if strings.HasPrefix(branch, "release/") {
+		version = strings.TrimPrefix(branch, "release/")
+		version = "beta" + ":" + version + ":" + build
+	} else if strings.HasPrefix(branch, "master") {
+		version = "rel" + ":" + version + ":" + build
+	} else {
+		version = "dev" + ":" + build
+	}
+	return version
+}
