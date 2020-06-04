93fb6a91f530744e65494bdbe1ab7c6bf8c7cf80
chaosch@163.com <ch740205>
2020-06-04 11:07:08 +0800

    增加 NewPrivateTracer

--- egov/opentracing-api/report.go
+++ egov/opentracing-api/report.go
@@ -38,4 +38,33 @@ func NewTracer(reportUrl, serviceName, endPoint *string) (reporter.Reporter, ope
 
 }
 
+func NewPrivateTracer(reportUrl, serviceName, endPoint *string) (reporter.Reporter, opentracing.Tracer) {
+	if reportUrl == nil || endPoint == nil {
+		return nil,nil
+	}
+	reporter := zipkinhttp.NewReporter(*reportUrl)
+
+	// create our local service endpoint
+	endpoint, err := zipkin.NewEndpoint(*serviceName, *endPoint)
+	if err != nil {
+		return nil,nil
+	}
+
+	// initialize our tracer
+	nativeTracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		return nil,nil
+	}
+
+	// use zipkin-go-opentracing to wrap our tracer
+	tracer := zipkinot.Wrap(nativeTracer)
+
+	// optionally set as Global OpenTracing tracer instance
+	//opentracing.SetGlobalTracer(tracer)
+
+	return reporter,tracer
+
+}
+
+
 /////////////////////////////////
